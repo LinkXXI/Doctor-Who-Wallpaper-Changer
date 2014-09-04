a3a3dae8658e265c27afd49255ad3b8b16cebb46
Kevin Lee <KevinRobertLee@gmail.com>
2014-09-03 21:28:34 -0400

    Commit nearly finished with dist.
    Docs still needed, very basic implementation.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 Doctor-Who-Wallpaper-Changer
+
 ============================
 
+
+
 C# program that changes wallpaper every hour to represent the doctor that relates to the hour.
+
+
+
+How to use
+
+----------
+
+
+
+The program does not run by it's self and requires a windows task to be created in order to change the wallpaper on time.
+
+A document will be added later describing the exact process.
---
+++ src/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DwWallpaperChanger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = "";
+            if (args.Length > 0)
+            {
+                type = args[0];
+            }
+
+            string path = "";
+
+            if(type == "-WarDoctor")
+            {
+                path = @".\withWar\";
+            }
+            else
+            {
+                path = @".\noWar\";
+            }
+
+            DateTime time = DateTime.Now;
+            Console.WriteLine(time.Hour);
+
+            Uri imagePath = new Uri("C:\\");
+
+            switch (time.Hour)
+            {
+                case 1: case 13:
+                    imagePath = new Uri(Path.GetFullPath(path + "01.jpg"));
+                    break;
+                case 2: case 14:
+                    imagePath = new Uri(Path.GetFullPath(path + "02.jpg"));
+                    break;
+                case 3: case 15:
+                    imagePath = new Uri(Path.GetFullPath(path + "03.jpg"));
+                    break;
+                case 4: case 16:
+                    imagePath = new Uri(Path.GetFullPath(path + "04.jpg"));
+                    break;
+                case 5: case 17:
+                    imagePath = new Uri(Path.GetFullPath(path + "05.jpg"));
+                    break;
+                case 6: case 18:
+                    imagePath = new Uri(Path.GetFullPath(path + "06.jpg"));
+                    break;
+                case 7: case 19:
+                    imagePath = new Uri(Path.GetFullPath(path + "07.jpg"));
+                    break;
+                case 8: case 20:
+                    imagePath = new Uri(Path.GetFullPath(path + "08.jpg"));
+                    break;
+                case 9: case 21:
+                    imagePath = new Uri(Path.GetFullPath(path + "09.jpg"));
+                    break;
+                case 10: case 22:
+                    imagePath = new Uri(Path.GetFullPath(path + "10.jpg"));
+                    break;
+                case 11: case 23:
+                    imagePath = new Uri(Path.GetFullPath(path + "11.jpg"));
+                    break;
+                case 0: case 12:
+                    imagePath = new Uri(Path.GetFullPath(path + "12.jpg"));
+                    break;
+            }
+
+            Wallpaper.Set(imagePath, Wallpaper.Style.Fill);
+        }
+    }
+}
